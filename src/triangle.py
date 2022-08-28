56e6a3ce653f7bf584be37845fb100209c8b3779
modengann <99417989+modengann@users.noreply.github.com>
2022-08-28 13:11:22 -0400

    init

---
+++ src/triangle.py
@@ -0,0 +1,15 @@
+'''Module containing functions calculating triangle properties'''
+
+# Enter you module contents here
+__author__ = 'Casey'
+__version__ = '1.0'
+
+
+import math
+def hypothenuse(length1, length2):
+    '''Returns the hypoteuse of right angled triangle when given lengths of two other sides'''
+    return math.sqrt(float(length1) ** 2 + float(length2) ** 2)
+    
+def area(side1,side2):
+    '''Returns the area of the right-angled triangle, when two sides, perpendicular to each other, are given as parameters.''' 
+    return 0.5 * float(side1) * float(side2)
