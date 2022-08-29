d6fd1a44118ed8721c23a255aa1946f264a0ced0
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:50:03 -0400

    Update triangle.py

--- src/triangle.py
+++ src/triangle.py
@@ -7,9 +7,7 @@ __version__ = '1.0'
 
 import math
 def hypothenuse(length1, length2):
-    '''Returns the hypoteuse of right angled triangle when given lengths of two other sides'''
-    return math.sqrt(float(length1) ** 2 + float(length2) ** 2)
+    pass
     
 def area(side1,side2):
-    '''Returns the area of the right-angled triangle, when two sides, perpendicular to each other, are given as parameters.''' 
-    return 0.5 * float(side1) * float(side2)
+    pass
